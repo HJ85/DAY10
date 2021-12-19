30940f3498bd00956cce20d859305d22c275439f
HJ85 <96196316+HJ85@users.noreply.github.com>
2021-12-19 15:36:57 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,62 @@
+myVariable = false;
+false
+if (myVariable === true) {
+console.log(`The conditon has been evaluated to true`);}
+undefined
+
+a = 7
+7
+
+if(a < 5) {
+console.log(a / 2);
+} else {
+console.log(a * 2);
+}
+
+a = 2;
+if(a < 5) {
+    console.log(a / 2);
+    } else {
+    console.log(a * 2);
+    }
+
+bookCount = 10;
+for (let i = 1; i <= bookCount; i++) {
+    console.log(i);
+}
+
+bookCount = 20;
+for (let i = 0; i <= bookCount; i++) {
+    console.log(i);
+}
+
+add =(a, b) => {return a + b;}
+console.log(add(1, 2))
+
+printNumbersTill = 20
+for (let i = 1; i <= printNumbersTill; i++) {
+    console.log(i);    
+}
+
+printNumbersTill = 15
+for (let i = 1; i <= printNumbersTill; i++) {
+    console.log(i);
+}
+
+let getGreetingTo = name => {
+    console.log(`getGreetingsTo, ${name}`)
+}
+getGreetingTo("Mark")
+
+printValues = [0, 3, 6, 7, 9]
+//* I donot know*//
+for (let i = 0; i < printValues.lenght; i++) {
+    console.log = printValues[i];    
+}
+
+myNumbers = [10, 20, 30, 50, 12]
+
+myNumbers.forEach(number => {
+    console.log(number);
+});
+
